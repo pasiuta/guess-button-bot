f30bf5056230ebce142689ed1fd183b969bb132e
vitaliypasuta <pasutavitaliy@pumplyx.com>
2023-08-30 12:16:58 +0300

    add game options

--- index.js
+++ index.js
@@ -1,28 +1,11 @@
 const telegramApi = require('node-telegram-bot-api')
 const token = '6534088077:AAFNUuBeDB0gl3hmGmPeWgHd0HvPzOt6mJM'
-
+const {gameOptions,againOptions} = require('/options.js')
 const bot = new telegramApi(token,{polling:true})
 
 const chats = {}
 
-const gameOptions = {
-    reply_markup:JSON.stringify({
-        inline_keyboard:[
-            [{text:'1',callback_data:'1'},{text:'2',callback_data:'2'},{text:'3',callback_data:'3'}],
-            [{text:'4',callback_data:'4'},{text:'5',callback_data:'5'},{text:'6',callback_data:'6'}],
-            [{text:'7',callback_data:'7'},{text:'8',callback_data:'8'},{text:'9',callback_data:'9'}],
-            [{text:'0',callback_data:'0'}],
-        ]
-    })
-}
 
-const againOptions = {
-    reply_markup:JSON.stringify({
-        inline_keyboard:[
-            [{text:'Play one more time',callback_data:'/again'}],
-        ]
-    })
-}
 
 const startGame = async (chatId) => {
     await bot.sendMessage(chatId,'I guess number from 0 to 9 and you should to guess this number')
